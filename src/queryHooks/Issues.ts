8d080ef8f2bf682caeaea34cf4e0eabd7da59f42
youridijk <steltje.maraboe_0q@icloud.com>
2023-06-08 19:12:06 +0200

    Use offset for other issues

--- src/queryHooks/Issues.ts
+++ src/queryHooks/Issues.ts
@@ -2,7 +2,7 @@ import settings from '../Settings';
 import {useQuery} from '@tanstack/react-query';
 import Issue from '../types/Issue';
 
-const url = `${settings.postgrestUrl}/issue_with_images?select=*,publication(title)&publicationcode=eq.nl/PO3&order=filledoldestdate.desc&limit=10&pages=neq.1`;
+const url = `${settings.postgrestUrl}/issue_with_images?select=*,publication(title)&publicationcode=eq.nl/PO3&order=filledoldestdate.desc&limit=10&offset=100&pages=neq.1`;
 
 export function useIssues() {
   return useQuery<Issue[], Error>({
