d9fc84b1a564f0860b29ade1ec3e4bd56e76c24d
youridijk <steltje.maraboe_0q@icloud.com>
2023-06-27 21:06:19 +0200

    Update .gitignore and meilisearch client

--- src/clients/Meilisearch.ts
+++ src/clients/Meilisearch.ts
@@ -1,7 +1,7 @@
 import { MeiliSearch } from 'meilisearch';
 import settings from '../Settings';
+import '@env';
 
-console.log('M ' + process.env.MEILISEARCH_SEARCH_KEY);
 const client = new MeiliSearch({
   host: settings.meilisearchUrl,
   apiKey: process.env.MEILISEARCH_SEARCH_KEY ?? '4456fb43dd0732f319d13d2ad1e04979fc61dd6ec593ad6dfa7bd530fa89c553',
