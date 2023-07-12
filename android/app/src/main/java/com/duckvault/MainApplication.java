15edff736ff6730cd17346c6e3ad63dc7f5830ce
youridijk <steltje.maraboe_0q@icloud.com>
2023-07-12 18:45:12 +0200

    Rename app to DuckVault

--- android/app/src/main/java/com/duckvault/MainApplication.java
+++ android/app/src/main/java/com/duckvault/MainApplication.java
@@ -1,4 +1,4 @@
-package com.inducks;
+package com.duckvault;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/DuckVaultTests/DuckVaultTests.m
+++ ios/DuckVaultTests/DuckVaultTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface inducksTests : XCTestCase
+@interface DuckVaultTests : XCTestCase
 
 @end
 
-@implementation inducksTests
+@implementation DuckVaultTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
